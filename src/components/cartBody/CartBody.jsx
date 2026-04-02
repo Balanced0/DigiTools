7211a294ece7c46934867df58a08f3b6588a7315
balanced0 <alvichowdhury013@gmail.com>
2026-04-02 10:48:46 +0600

    Added empty card section

--- src/components/cartBody/CartBody.jsx
+++ src/components/cartBody/CartBody.jsx
@@ -6,6 +6,7 @@ import Portfolio from '../../assets/products/portfolio.png'
 import ShoppingCart from '../../assets/products/shopping-cart.png'
 import SocialMedia from '../../assets/products/social-media.png'
 import Camera from '../../assets/products/camera.png'
+import { FiShoppingCart } from "react-icons/fi";
 
 const iconsList = {
     ai:  Writing,
@@ -20,36 +21,43 @@ const CartBody = ({cartItems, cost, setCost}) => {
     return (
         <div class="container mx-auto mb-32">
             <div className="card bg-base-100 shadow-sm">
-            <div className="card-body">
-                <h2 className="card-title mb-6">Your Cart</h2>
-                <div className="mb-4">
-                    {
-                        cartItems.map((item) =>{
-                            return(
-                                <div className="card card-body bg-base-200 mb-6">
-                                    <div className="flex justify-between items-center">
-                                        <div className="flex gap-4 items-center justify-center">
-                                            <div className="border border-neutral-200 w-13 h-13 rounded-full flex items-center justify-center">
-                                                <img src={iconsList[item.icon]}></img>
-                                            </div>
-                                            <div>
-                                                <p className="font-semibold text-xl mb-2">{item.name}</p>
-                                                <p className="font-medium text-[#627382]">${item.price}</p>
+                {
+                    cartItems.length === 0?
+                    (
+                        <div className="card-body"> <h2 className="card-title mb-6">Your Cart</h2> <div className="flex justify-center items-center flex-col"> <FiShoppingCart className="mb-4 text-[#627382] mt-6" size={46}/> <p className="mb-28 font-bold text-[#627382] text-lg">Your cart is empty</p> </div> </div>
+                    ):(
+                        <div className="card-body">
+                        <h2 className="card-title mb-6">Your Cart</h2>
+                        <div className="mb-4">
+                            {
+                                cartItems.map((item) =>{
+                                    return(
+                                        <div key={item.id} className="card card-body bg-base-200 mb-6">
+                                            <div className="flex justify-between items-center">
+                                                <div className="flex gap-4 items-center justify-center">
+                                                    <div className="border border-neutral-200 w-13 h-13 rounded-full flex items-center justify-center">
+                                                        <img src={iconsList[item.icon]}></img>
+                                                    </div>
+                                                    <div>
+                                                        <p className="font-semibold text-xl mb-2">{item.name}</p>
+                                                        <p className="font-medium text-[#627382]">${item.price}</p>
+                                                    </div>
+                                                </div>
+                                                <div className="text-[#FF3980] font-bold cursor-pointer">Remove</div>
                                             </div>
                                         </div>
-                                        <div className="text-[#FF3980] font-bold cursor-pointer">Remove</div>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-                <div className="flex justify-between items-center mb-7">
-                    <div className="text-[##627382]">Total:</div>
-                    <div className="font-bold text-2xl">${cost}</div>
-                </div>
-                <button className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white font-bold py-4 rounded-full">Proceed to Checkout</button>
-            </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className="flex justify-between items-center mb-7">
+                            <div className="text-[#627382]">Total:</div>
+                            <div className="font-bold text-2xl">${cost}</div>
+                        </div>
+                        <button className="btn bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white font-bold py-4 rounded-full">Proceed to Checkout</button>
+                    </div>
+                    )
+                }
             </div>
         </div>
     );
--- src/components/toolCardBody/ToolCardBody.jsx
+++ src/components/toolCardBody/ToolCardBody.jsx
@@ -7,7 +7,7 @@ const ToolCardBody = ({toolsPromise, cartItems, setCartItems, cost, setCost}) =>
         <div className="container mx-auto px-4 mb-32 items-stretch gap-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {
                 data.map((tool) => {
-                    return <ToolCard key={tool.id} tool={tool} cartItems={cartItems} setCartItems={setCartItems} cost={cost} setCost={setCost}></ToolCard>;
+                            return <ToolCard key={tool.id} tool={tool} cartItems={cartItems} setCartItems={setCartItems} cost={cost} setCost={setCost}></ToolCard>;
                 })
             }
         </div>
